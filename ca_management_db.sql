29e780dd4716f7104cfc4fdfb11a593e4faded3c
Jemar Jude Maranga <Jemar Jude Maranga>
2017-01-01 22:26:40 +0800

    ca_management_db.sql

--- ca_management_db.sql
+++ ca_management_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 26, 2016 at 03:29 AM
+-- Generation Time: Jan 01, 2017 at 03:25 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 7.0.6
 
@@ -19,6 +19,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `ca_management_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `announcement`
+--
+
+CREATE TABLE `announcement` (
+  `AnnouncementID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `DatePosted` datetime DEFAULT CURRENT_TIMESTAMP,
+  `Subject` varchar(40) DEFAULT NULL,
+  `Message` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `announcement`
+--
+
+INSERT INTO `announcement` (`AnnouncementID`, `UserID`, `DatePosted`, `Subject`, `Message`) VALUES
+(31, 1, '2017-01-01 18:08:18', 'CodeAxis', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non dolor ac velit molestie bibendum sed vitae lectus. Donec vitae efficitur nibh, ut semper dui. Sed sit amet odio eu dui scelerisque placerat. Pellentesque a ultricies tortor. Cras iaculis condimentum sapien, nec efficitur est molestie vel. Vestibulum cursus, nibh hendrerit convallis dignissim, neque nulla accumsan lacus, ac egestas ante ante a dolor. Morbi sagittis, elit eget tempus pretium, quam nunc dapibus lectus, sed sollicitudin lacus urna vel enim. Pellentesque a elementum mauris, viverra pellentesque magna. Sed fringilla lacinia tempus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas vulputate velit ut nibh ornare, vel gravida tortor congue. Nam euismod in libero id ultrices. Etiam vulputate fringilla nisi et mattis. Vivamus accumsan in orci nec efficitur.'),
+(32, 1, '2017-01-01 18:08:25', 'Meeting this Saturday', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non dolor ac velit molestie bibendum sed vitae lectus. Donec vitae efficitur nibh, ut semper dui. Sed sit amet odio eu dui scelerisque placerat. Pellentesque a ultricies tortor. Cras iaculis condimentum sapien, nec efficitur est molestie vel. Vestibulum cursus, nibh hendrerit convallis dignissim, neque nulla accumsan lacus, ac egestas ante ante a dolor. Morbi sagittis, elit eget tempus pretium, quam nunc dapibus lectus, sed sollicitudin lacus urna vel enim. Pellentesque a elementum mauris, viverra pellentesque magna. Sed fringilla lacinia tempus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas vulputate velit ut nibh ornare, vel gravida tortor congue. Nam euismod in libero id ultrices. Etiam vulputate fringilla nisi et mattis. Vivamus accumsan in orci nec efficitur.'),
+(43, 1, '2017-01-01 21:05:28', 'This is a test.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non dolor ac velit molestie bibendum sed vitae lectus. Donec vitae efficitur nibh, ut semper dui. Sed sit amet odio eu dui scelerisque placerat. Pellentesque a ultricies tortor. Cras iaculis condimentum sapien, nec efficitur est molestie vel. Vestibulum cursus, nibh hendrerit convallis dignissim, neque nulla accumsan lacus, ac egestas ante ante a dolor. Morbi sagittis, elit eget tempus pretium, quam nunc dapibus lectus, sed sollicitudin lacus urna vel enim. Pellentesque a elementum mauris, viverra pellentesque magna. Sed fringilla lacinia tempus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas vulputate velit ut nibh ornare, vel gravida tortor congue. Nam euismod in libero id ultrices. Etiam vulputate fringilla nisi et mattis. Vivamus accumsan in orci nec efficitur.'),
+(46, 1, '2017-01-01 21:45:27', 'sdf', 'sdf'),
+(47, 1, '2017-01-01 21:45:33', 'sdf', 'sdf');
 
 -- --------------------------------------------------------
 
@@ -44,7 +69,8 @@ INSERT INTO `message` (`MessageID`, `UserID`, `Message`, `Subject`, `date`, `vie
 (3, 2, 'Message', 'Subject', '2016-12-26 01:14:54', b'0', 1),
 (4, 2, 'kaon na didto oy nimal', 'Kaon na', '2016-12-26 09:31:04', b'0', 1),
 (5, 2, 'Tuon na didto oy nimal', 'Tuon na', '2016-12-26 09:31:16', b'0', 1),
-(6, 2, 'Ngano weak kayka oyy zzz sohai la', 'Weak', '2016-12-26 09:31:28', b'0', 1);
+(6, 2, 'Ngano weak kayka oyy zzz sohai la', 'Weak', '2016-12-26 09:31:28', b'0', 1),
+(7, 5, 'ngano weak man ka?!', 'Weak', '2016-12-26 22:41:31', b'0', 1);
 
 -- --------------------------------------------------------
 
@@ -85,6 +111,13 @@ INSERT INTO `user` (`UserID`, `UserType`, `FirstName`, `LastName`, `Gender`, `Ag
 --
 
 --
+-- Indexes for table `announcement`
+--
+ALTER TABLE `announcement`
+  ADD PRIMARY KEY (`AnnouncementID`),
+  ADD KEY `announcement_fk` (`UserID`);
+
+--
 -- Indexes for table `message`
 --
 ALTER TABLE `message`
@@ -102,10 +135,15 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `announcement`
+--
+ALTER TABLE `announcement`
+  MODIFY `AnnouncementID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+--
 -- AUTO_INCREMENT for table `message`
 --
 ALTER TABLE `message`
-  MODIFY `MessageID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `MessageID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `user`
 --
@@ -114,6 +152,12 @@ ALTER TABLE `user`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `announcement`
+--
+ALTER TABLE `announcement`
+  ADD CONSTRAINT `announcement_fk` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`);
 
 --
 -- Constraints for table `message`
